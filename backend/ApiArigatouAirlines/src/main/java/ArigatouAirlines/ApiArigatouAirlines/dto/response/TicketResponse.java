2e396f0ed60c0824543c1c42508a861b4236e985
DaoLeLongVu <workindllv@gmail.com>
2025-12-28 15:00:59 +0700

    Add ID fields to response DTOs
    Introduced new ID fields (AncillaryServiceId, ticketId, voucherId) to AncillaryServiceResponse, TicketResponse, and VoucherResponse classes to include unique identifiers in API responses.

--- backend/ApiArigatouAirlines/src/main/java/ArigatouAirlines/ApiArigatouAirlines/dto/response/TicketResponse.java
+++ backend/ApiArigatouAirlines/src/main/java/ArigatouAirlines/ApiArigatouAirlines/dto/response/TicketResponse.java
@@ -15,6 +15,8 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TicketResponse {
+    int ticketId;
+
 
     BookingResponse booking;
 
